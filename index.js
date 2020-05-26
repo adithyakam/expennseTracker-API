562a8f34729218ca2bc9d8283e0ee2beac6c5538
adithyakam <50355901+adithyakam@users.noreply.github.com>
2020-05-26 16:03:43 +0530

    deploying

--- index.js
+++ index.js
@@ -12,10 +12,8 @@ const del = require("./Controller/Delete");
 const postgres = knex({
   client: "pg",
   connection: {
-    host: "127.0.0.1",
-    user: "postgres",
-    password: "root",
-    database: "Expense",
+    connectionString: process.env.DATABASE_URL,
+    ssl: true,
   },
 });
 
